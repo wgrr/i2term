0fdb2126c37cfad54aa51fd7e46ba95730a67538
Wagner Riffel <wgrriffel@gmail.com>
2019-01-26 15:33:32 -0200

    all: it should print image size in characters not pixels.

--- main.go
+++ main.go
@@ -4,20 +4,26 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"fmt"
 	"image"
 	"image/gif"
 	"image/jpeg"
 	"image/png"
+	"math"
 	"os"
 	"path/filepath"
-	"regexp"
 
 	"golang.org/x/sys/unix"
 )
 
 const usagestr = `lukeidraw: usage: [ file ]
 `
+
+var (
+	charwScale = 1.0
+	charhScale = 1.0
+)
 
 func main() {
 	if len(os.Args) < 2 {
@@ -56,6 +62,23 @@ func main() {
 		}
 	}
 
+	win, err := unix.IoctlGetWinsize(1, unix.TIOCGWINSZ)
+	if err != nil {
+		fatal(err.Error())
+	}
+	// just being paranoid about kernel input, im not being about terminal
+	// input because math.Ceil will round up character pixel dimensions, no
+	// div by 0 possible there.
+	if win.Row == 0 {
+		win.Row = 1
+	}
+	if win.Col == 0 {
+		win.Col = 1
+	}
+
+	charw := int(math.Ceil((float64(win.Ypixel) * charwScale) / float64(win.Row)))
+	charh := int(math.Ceil((float64(win.Xpixel) * charhScale) / float64(win.Col)))
+
 	var term unix.Termios
 	tmp, err := unix.IoctlGetTermios(0, unix.TCGETS)
 	if err != nil {
@@ -73,26 +96,26 @@ func main() {
 	r := bufio.NewReader(os.Stdin)
 	termin, err := r.ReadBytes(byte('R'))
 	if err != nil {
-		// try to not break currenet terminal
+		// try restore user terminal
 		unix.IoctlSetTermios(0, unix.TCSETS, tmp)
+		fatal("unexpected input from terminal")
+	}
+
+	tmpbuf := bytes.NewBuffer(termin[2:])
+	row, err := tmpbuf.ReadBytes(byte(';'))
+	if err != nil {
+		fatal("unexpected input from terminal")
+	}
+	col, err := tmpbuf.ReadBytes(byte('R'))
+	if err != nil {
 		fatal("unexpected input from terminal")
 	}
 
 	err = unix.IoctlSetTermios(0, unix.TCSETS, tmp)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "reset: %s\nyour terminal is broken, fix it manually by typing reset", err)
+		fmt.Fprintf(os.Stderr, "lukeidraw: %s\nyour terminal is broken, fix it manually by typing reset", err)
 	}
-
-	pattern, err := regexp.Compile("[0-9]+;[0-9]+")
-	if err != nil {
-		// should never reach
-		fatal(err.Error())
-	}
-	tpos := pattern.FindAll(termin, -1)
-	if tpos == nil {
-		fatal("unexpected input from terminal")
-	}
-	fmt.Printf("%d;%d-%s\n", cfg.Width, cfg.Height, tpos[0])
+	fmt.Printf("%d %d %s %s\n", cfg.Width/charw, cfg.Height/charh, string(row[:len(row)-1]), string(col[:len(col)-1]))
 }
 
 // noreturn
