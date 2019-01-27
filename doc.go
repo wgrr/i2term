c63000add5de3e33c6809c64f7515f0326febdeb
Wagner Riffel <wgrriffel@gmail.com>
2019-01-27 13:47:36 -0200

    refactor: give it a name and meaning.

--- doc.go
+++ doc.go
@@ -1,14 +1,11 @@
 /*
 
-lukeidraw takes an image file (it supports jpeg, png, gif, bmp, tiff,
-vp8l and webp encodings) and prints a formated output that has image
-dimensions and current terminal position in terminal character size.
-The output is meant to be parsed by external programs, its output is
-formated as following:
-	imageWidth imageHeight termCurrentRow termCurrentColumn
+i2term takes an image file (it supports jpeg, png, gif, bmp, tiff,
+vp8l and webp encodings) and prints image dimensions in terminal
+character size.
 
-Example:
-	lukeidraw img.png # 30 96 39 1
+Its output  is formated as following:
+	imageWidth imageHeight
 
 */
 package main
--- main.go
+++ main.go
@@ -3,8 +3,6 @@
 package main
 
 import (
-	"bufio"
-	"bytes"
 	"errors"
 	"flag"
 	"fmt"
@@ -73,7 +71,7 @@ func main() {
 			fatal(err.Error())
 		}
 	}
-	win, err := unix.IoctlGetWinsize(1, unix.TIOCGWINSZ)
+	win, err := unix.IoctlGetWinsize(0, unix.TIOCGWINSZ)
 	if err != nil {
 		fatal(err.Error())
 	}
@@ -82,49 +80,14 @@ func main() {
 	winrow, winpxrow := math.Max(float64(win.Row), 1.0), math.Max(float64(win.Ypixel), 1.0)
 	wincol, winpxcol := math.Max(float64(win.Col), 1.0), math.Max(float64(win.Xpixel), 1.0)
 
+	// user input, avoid div by 0
 	fontsclw := math.Max(*charwScale, 0.01)
 	fontsclh := math.Max(*charhScale, 0.01)
 
 	charw := int(math.Ceil((winpxrow * fontsclw) / winrow))
 	charh := int(math.Ceil((winpxcol * fontsclh) / wincol))
 
-	var term unix.Termios
-	tmp, err := unix.IoctlGetTermios(0, unix.TCGETS)
-	if err != nil {
-		fatal("couldn't setup termio to listen to terminal input: " + err.Error())
-	}
-
-	term = *tmp
-	term.Lflag &= (^uint32(unix.ICANON) & ^uint32(unix.ECHO))
-	err = unix.IoctlSetTermios(0, unix.TCSETS, &term)
-	if err != nil {
-		fatal("couldn't setup termio to listen to terminal input: " + err.Error())
-	}
-
-	os.Stdout.WriteString("\x1b[6n")
-	r := bufio.NewReader(os.Stdin)
-	termin, err := r.ReadBytes(byte('R'))
-	if err != nil {
-		// try restore user terminal
-		unix.IoctlSetTermios(0, unix.TCSETS, tmp)
-		fatal("unexpected input from terminal")
-	}
-
-	tmpbuf := bytes.NewBuffer(termin[2:])
-	row, err := tmpbuf.ReadBytes(byte(';'))
-	if err != nil {
-		fatal("unexpected input from terminal")
-	}
-	col, err := tmpbuf.ReadBytes(byte('R'))
-	if err != nil {
-		fatal("unexpected input from terminal")
-	}
-
-	err = unix.IoctlSetTermios(0, unix.TCSETS, tmp)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "lukeidraw: %syour terminal is broken, fix it manually by typing reset\n", err)
-	}
-	fmt.Printf("%d %d %s %s\n", cfg.Width/charw, cfg.Height/charh, string(row[:len(row)-1]), string(col[:len(col)-1]))
+	fmt.Printf("%d %d\n", cfg.Width/charw, cfg.Height/charh)
 }
 
 // noreturn
